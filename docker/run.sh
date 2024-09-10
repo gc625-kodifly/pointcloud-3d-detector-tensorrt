822a13716dbbfad1add5056c1a58ca580d28f82d
gc625 <gabrielchan625@gmail.com>
2024-09-10 15:58:30 +0800

    update run script to recursively find ws dir

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")") # current dir .../iassd_trt_ws/src/pointcloud-3d-detector-tensorrt/docker
-WORKSPACE_DIR=$(readlink -f "$SCRIPT_DIR/../../..") # workspace dir .../iassd_trt_ws/
+WORKSPACE_DIR=$SCRIPT_DIR 
+# Traverse up the directory tree until .catkin_workspace is found
+while [ "$WORKSPACE_DIR" != "/" ]; do
+    echo checking ${WORKSPACE_DIR}
+    if [ -e "$WORKSPACE_DIR/.catkin_workspace" ]; then
+        break  # Found the .catkin_workspace file
+    fi
+    WORKSPACE_DIR=$(dirname "$WORKSPACE_DIR")  # Move one directory up
+done
 
+# If not found, error out
+if [ ! -e "$WORKSPACE_DIR/.catkin_workspace" ]; then
+    echo "Error: .catkin_workspace file not found in any parent directories."
+    exit 1
+fi
 # echo ${PROJECT_DIR}
 
 DOCKER_ARGS+=("-v /tmp/.X11-unix:/tmp/.X11-unix")
